721c363d7070f59b69e6a6b1095a6c75e4898c97
marripallymounika62 <135951998+marripallymounika62@users.noreply.github.com>
2024-06-11 15:31:25 +0530

    Add files via upload

---
+++ src/assets/becometutorimage/index.js
@@ -0,0 +1,9 @@
+import image10 from './image 10.svg';
+import imagehandsome from './boy.svg';
+import logofb from './logo-fb-simple 2.svg';
+import github from './logo-github 1.svg';
+import logoinsta from './logo-instagram 1.svg';
+import logotwitter from './logo-twitter 2.svg';
+import Sophiemoore from './SophieMoore.svg'
+
+export { image10, imagehandsome, logofb, github, logoinsta, logotwitter, Sophiemoore}
